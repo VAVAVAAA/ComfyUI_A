1495d9c860e9e47192534044dcf03c80abd8cb73
VAVAVAAA <99593810+VAVAVAAA@users.noreply.github.com>
2024-08-30 18:04:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+os.system('sudo apt-get update')
+os.system('sudo apt-get install -y aria2')
+
 os.system(f"git lfs install")
 # os.system(f"git clone https://github.com/comfyanonymous/ComfyUI /home/xlab-app-center/ComfyUI")
 os.chdir(f"/home/xlab-app-center")
