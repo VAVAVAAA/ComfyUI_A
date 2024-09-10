6fab76fb55a25a35e9a143378e5a0b3868bf7e2a
VAVAVAAA <99593810+VAVAVAAA@users.noreply.github.com>
2024-09-10 20:22:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ os.system("pip install transparent_background")
 os.system("pip install rembg")
 os.system("pip install piexif")
 os.system("pip install accelerate>=0.25.0")
+os.system("pip install blend_modes")
+
 
 # os.chdir(f"/home/xlab-app-center")
 # 插件
