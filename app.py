e7a07d9494e5d843f0fb77a9057e9bbaaad2818e
VAVAVAAA <99593810+VAVAVAAA@users.noreply.github.com>
2024-09-20 15:33:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ os.system("pip install rembg")
 os.system("pip install piexif")
 os.system("pip install accelerate>=0.25.0")
 os.system("pip install blend_modes")
-
+os.system("pip install diffusers")
 
 # os.chdir(f"/home/xlab-app-center")
 # 插件
